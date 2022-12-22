f6b7536b9a9b8310284d578035b3b82fca0bcbf2
HaydenHo <hayden.ho23@ycdsbk12.ca>
2022-12-22 10:03:05 -0500

    code

---
+++ src/Song.java
@@ -0,0 +1,45 @@
+public class Song {
+    private String title;
+    private String artist;
+    private int duration;
+    private Album album;
+  
+    public Song(String title, String artist, int duration, Album album) {
+      this.title = title;
+      this.artist = artist;
+      this.duration = duration;
+      this.album = album;
+    }
+  
+    public String getTitle() {
+      return this.title;
+    }
+  
+    public void setTitle(String title) {
+      this.title = title;
+    }
+  
+    public String getArtist() {
+      return this.artist;
+    }
+  
+    public void setArtist(String artist) {
+      this.artist = artist;
+    }
+  
+    public int getDuration() {
+      return this.duration;
+    }
+  
+    public void setDuration(int duration) {
+      this.duration = duration;
+    }
+  
+    public Album getAlbum() {
+      return this.album;
+    }
+  
+    public void setAlbum(Album album) {
+      this.album = album;
+    }
+  }

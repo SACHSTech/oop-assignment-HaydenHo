7a91d3782cfa94d7814902a27d7161897a4913d0
HaydenHo <hayden.ho23@ycdsbk12.ca>
2022-12-22 10:04:23 -0500

    code

--- src/UserPlaylist.java
+++ src/UserPlaylist.java
@@ -1,6 +1,33 @@
-import java.io.*;
-import java.util.*;
-
 public class UserPlaylist {
-    
-}
+    private String name;
+    private List<Song> songs;
+  
+    public UserPlaylist(String name, List<Song> songs) {
+      this.name = name;
+      this.songs = songs;
+    }
+  
+    public String getName() {
+      return this.name;
+    }
+  
+    public void setName(String name) {
+      this.name = name;
+    }
+  
+    public List<Song> getSongs() {
+      return this.songs;
+    }
+  
+    public void setSongs(List<Song> songs) {
+      this.songs = songs;
+    }
+  
+    public void addSong(Song song) {
+      this.songs.add(song);
+    }
+  
+    public void removeSong(Song song) {
+      this.songs.remove(song);
+    }
+  }

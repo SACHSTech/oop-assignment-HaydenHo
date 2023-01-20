77ad7283e91497dfee47ed972e57f975c1ba8369
HaydenHo <hayden.ho23@ycdsbk12.ca>
2023-01-20 14:28:01 -0500

    code

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,29 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static BufferedReader keyboard;
+    public static void main (String[] args) throws IOException {
+    // Setting Variable and object 
+    String strUser;
+    String strChoice;
+    int intSong;
+    int intSlot;
+    Scanner sc = new Scanner(System.in);        
+    boolean blnMenu = true;
+    boolean blnList = false;
+    ArrayList<String> MediaList = new ArrayList<String>();
+    ArrayList<String> genreList = new ArrayList<String>();
+    ArrayList<Media> objMediaList = new ArrayList<Media>();
+
+    SpotifyList newSpotifyList = new SpotifyList();
+
+    System.out.println("Welcome to your Spotify list! What is your name?:");
+    strUser = sc.nextLine();
+    System.out.println("Hi " + strUser + "! Here are your songs.");
+    System.out.println();
+
     
+    }
 }

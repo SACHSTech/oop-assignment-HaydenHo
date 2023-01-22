438143acdf4d8b8d61a34a5aacbb6b8a81cdd0a6
HaydenHo <hayden.ho23@ycdsbk12.ca>
2023-01-21 22:26:03 -0500

    code

---
+++ src/Food.java
@@ -0,0 +1,37 @@
+abstract class Food {
+    private String name;
+    private double price;
+    private String ingredients;
+
+    public Food(String name, double price, String ingredients) {
+        this.name = name;
+        this.price = price;
+        this.ingredients = ingredients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public abstract void cook();
+}
---
+++ src/Order.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+class Order {
+  private String customerName;
+  private String customerAddress;
+  private String customerPhone;
+  private ArrayList<Food> foodList;
+
+  public Order(String customerName, String customerAddress, String customerPhone) {
+    this.customerName = customerName;
+    this.customerAddress = customerAddress;
+    this.customerPhone = customerPhone;
+    this.foodList = new ArrayList<Food>();
+  }
+
+  public void addFoodToOrder(Food food) {
+    foodList.add(food);
+  }
+
+  public void removeFoodFromOrder(Food food) {
+    foodList.remove(food);
+  }
+
+  public double calculateTotalCost() {
+    double totalCost = 0;
+    for (Food food : foodList) {
+      totalCost += food.getPrice();
+    }
+    return totalCost;
+  }
+
+  public void printOrderSummary() {
+    System.out.println("Order summary:");
+    System.out.println("Customer name: " + customerName);
+    System.out.println("Customer address: " + customerAddress);
+    System.out.println("Customer phone: " + customerPhone);
+    System.out.println("Food items:");
+    for (Food food : foodList) {
+      System.out.println("- " + food.getName());
+    }
+  }
+}

cd248cf731deea1978a908ae4e69e31d0f3029fe
HaydenHo <hayden.ho23@ycdsbk12.ca>
2023-01-21 20:53:25 -0500

    code

--- src/SpotifyList.java
+++ src/SpotifyList.java
@@ -3,17 +3,17 @@ public class SpotifyList {
     private ArrayList<Song> songList = new ArrayList<Song>();
     private ArrayList<Podcasts> podcastList = new ArrayList<Podcasts>();
 
-    Song Sunroof = new Song("Sunroof", "Nicky Youre, dazy", 169);
-    Song AsItWas = new Song("As It Was", "Harry Styles", 167);
-    Song CallMeMaybe = new Song("Call Me Maybe", "Carley Rae Jepsen", 193);
-    Song ThinkingOutLoud = new Song("Thinking Out Loud", "Ed Sheeran", 281);
-    Song Enemy = new Song("Enemy", "Imagine Dragons", 173);
+    Song Sunroof = new Song("Sunroof", "Nicky Youre, dazy", 169, 1000);
+    Song AsItWas = new Song("As It Was", "Harry Styles", 167, 3000);
+    Song CallMeMaybe = new Song("Call Me Maybe", "Carley Rae Jepsen", 193, 1000000);
+    Song ThinkingOutLoud = new Song("Thinking Out Loud", "Ed Sheeran", 281, 2000000);
+    Song Enemy = new Song("Enemy", "Imagine Dragons", 173, 9000);
 
-    Podcasts JoeRogan = new Podcasts("The Joe Rogan Experience", "Podcasts", "January 10",  "Joe Rogan", 142);
-    Podcasts TEDTalk = new Podcasts("Ted Talks Daily", "Postcasts", "January 10", "TED", 29);
-    Podcasts IceCoffee = new Podcasts("The Iced Coffee Hour", "Podcasts", "January 9", "Graham Stephen/Jack Selby", 66);
-    Podcasts OffThePill = new Podcasts("Off The Pill", "Podcasts", "December 15", "Higa TV", 51);
-    Podcasts JordanPeterson = new Podcasts("The Jordan B. Peterson Podcast", "Podcasts", "January 9", "Dr. Jordan B. Peterson", 114);
+    Podcasts JoeRogan = new Podcasts("The Joe Rogan Experience", "Podcasts", "January 10",  "Joe Rogan", 142, 1000);
+    Podcasts TEDTalk = new Podcasts("Ted Talks Daily", "Postcasts", "January 10", "TED", 29, 80);
+    Podcasts IceCoffee = new Podcasts("The Iced Coffee Hour", "Podcasts", "January 9", "Graham Stephen/Jack Selby", 66, 100);
+    Podcasts OffThePill = new Podcasts("Off The Pill", "Podcasts", "December 15", "Higa TV", 51, 400);
+    Podcasts JordanPeterson = new Podcasts("The Jordan B. Peterson Podcast", "Podcasts", "January 9", "Dr. Jordan B. Peterson", 114, 100000);
 
 
     public SpotifyList(){
